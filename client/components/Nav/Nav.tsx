be6fe850925da293a51fd8543782951cb37f7749
Jane Zhang <jiejanezhang2012@gmail.com>
2023-06-28 21:42:06 +1200

    added Nav component

---
+++ client/components/Nav/Nav.tsx
@@ -0,0 +1,54 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { useNavigate } from 'react-router-dom'
+
+interface Props {
+  toggleMenu: () => void
+}
+
+function Nav(props: Props) {
+  const { isAuthenticated, logout } = useAuth0()
+  const navigate = useNavigate()
+
+
+  function handleLogout() {
+    logout({ logoutParams: { returnTo: window.location.origin } })
+  }
+
+  function goTo(link: string) {
+    props.toggleMenu()
+    navigate(link)
+  }
+
+  return (
+    <nav className="pt-16 pl-4 flex">
+      <ul className="text-3xl">
+        <li>
+          <button onClick={() => goTo('/')}>Home</button>
+        </li>
+        <li>
+          <button onClick={() => goTo('/:location/activities')}>Activities</button>
+        </li>
+        <li>
+          <button onClick={() => goTo('/:location/classifieds')}>Classifieds</button>
+        </li>
+        <li>
+          <button onClick={() => goTo('/:location/market')}>Market</button>
+        </li>
+        <li>
+          <button onClick={() => goTo('/profile')}>Profile</button>
+        </li>
+        <li>
+          <button onClick={() => goTo('/my-posts')}>My posts</button>
+        </li>
+        {/* <li>
+          {!isAuthenticated && <button onClick={handleLogin}>Log in</button>}
+        </li> */}
+        <li>
+          {isAuthenticated && <button onClick={handleLogout}>Sign out</button>}
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
