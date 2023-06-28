be947a2b92724216776b9ed39834c73949746d38
Jaelyn <leejay1020@gmail.com>
2023-06-29 10:12:50 +1200

    create LoginButton Component

---
+++ client/Pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+function Home () {
+  return (
+    <div>
+      <p>Slogan will be located here</p>
+      <LoginButton />
+      <RegisterButton />
+    </div>
+  )
+}
+
+export default Home
--- client/components/Home/Home.tsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <p>This will be our home page with log in and register button.</p>
-}
-
-export default Home
