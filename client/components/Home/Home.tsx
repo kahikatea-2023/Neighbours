1a8ba40d1e2032490cf0d14dd0cb47395aa7118c
Jaelyn <leejay1020@gmail.com>
2023-06-28 17:25:35 +1200

    Create Home component

---
+++ client/components/Home/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <p>This will be our home page with log in and register button.</p>
+}
+
+export default Home
--- client/index.tsx
+++ client/index.tsx
@@ -8,28 +8,25 @@ import {
 } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Auth0Provider } from '@auth0/auth0-react'
+import AppLayout from './components/AppLayout/AppLayout'
+import Home from './components/Home/Home'
 
-
-export const routes = createRoutesFromElements (
-  <Route path='/' element={<AppLayout />}>
-    <Route index element={<Home />}/>
-    <Route path='register' element={<p>Register will be here</p>}/>
-    <Route path=':location' element={<p>Community will be here</p>}>
-      <Route path='activities' element={<p>Activities will be here</p>}/>
-      <Route path='classifieds' element={<p>Classifieds will be here</p>}>
-        <Route path=':id' element={<p>ClassifiledsItem will be here</p>}/>
+export const routes = createRoutesFromElements(
+  <Route path="/" element={<AppLayout />}>
+    <Route index element={<Home />} />
+    <Route path="register" element={<p>Register will be here</p>} />
+    <Route path=":location" element={<p>Community will be here</p>}>
+      <Route path="activities" element={<p>Activities will be here</p>} />
+      <Route path="classifieds" element={<p>Classifieds will be here</p>}>
+        <Route path=":id" element={<p>ClassifiledsItem will be here</p>} />
       </Route>
-      <Route path='market' element={<p>Market will be here</p>}/>
+      <Route path="market" element={<p>Market will be here</p>} />
     </Route>
-    <Route path='profile' element={<p>profile will be here</p>}/>
-    <Route path='my-posts' element={<p>posts will be here</p>}/> 
-
+    <Route path="profile" element={<p>profile will be here</p>} />
+    <Route path="my-posts" element={<p>posts will be here</p>} />
   </Route>
 )
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(
-    <App />
-  );
-});
+  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
+})
