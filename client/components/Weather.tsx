8dd9b346761adeea3db15482b61667b41b8e468f
Carlos Aguilar <aguilar.pinero@gmail.com>
2023-06-29 10:12:46 +1200

    update branch from main

--- client/components/Weather.tsx
+++ client/components/Weather.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useLocation } from 'react-router-dom'
-import { API_KEY } from './config'
+import { API_KEY } from '../../config'
 
 interface WeatherData {
   name: string
