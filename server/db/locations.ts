e4a7c25663e5bed1c6a2c5c43897689e5246248c
Sarah <sarahchungwu@gmail.com>
2023-06-29 16:13:21 +1200

    getLocationByID db funtion made

--- server/db/locations.ts
+++ server/db/locations.ts
@@ -1,6 +1,10 @@
 import db from './connection'
 import { LocationsDataBackend } from '../../models/locations'
 
-export async function getLocations() {
+export async function getAllLocations() {
   return (await db('locations').select('id', 'name')) as LocationsDataBackend[]
+}
+
+export async function getLocationById(id: number) {
+  return await db('locations').where('id', id).select('id', 'name').first() as LocationsDataBackend[]
 }
--- server/routes/locations.ts
+++ server/routes/locations.ts
@@ -8,9 +8,21 @@ const router = Router()
 
 router.get('/', async (req, res) => {
   try {
-    const locations = await db.getLocations()
+    const locations = await db.getAllLocations()
 
     res.json({ locations })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const location = await db.getLocationById(id)
+
+    res.json({ location })
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
