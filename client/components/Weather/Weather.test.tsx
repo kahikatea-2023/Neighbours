4cc09f7f2b07e180bd2c77f701a0e144c902ea25
Carlos Aguilar <aguilar.pinero@gmail.com>
2023-06-29 20:12:21 +1200

    2fix merge

--- client/components/Weather/Weather.test.tsx
+++ client/components/Weather/Weather.test.tsx
@@ -1,9 +1,16 @@
-import { render } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import Weather from './Weather'
 
-test('renders weather information', () => {
-  // Render the Weather component
-  render(<Weather />)
+test('should handle error when fetching weather data', async () => {
+  // Mock the API call to simulate an error
+  jest
+    .spyOn(global, 'fetch')
+    .mockRejectedValueOnce(new Error('Failed to fetch weather data'))
 
-  // Add your assertions here
+  await act(async () => {
+    render(<Weather />)
+  })
+
+  // Assert that the error message is rendered
+  expect(screen.getByText(/Error fetching weather data/i)).toBeInTheDocument()
 })
