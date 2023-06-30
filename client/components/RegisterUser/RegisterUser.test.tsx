90b31fd5ee0329612f7a18b3af3f50c51084cef9
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-06-30 12:30:40 +1200

    test setup done

--- client/components/RegisterUser/RegisterUser.test.tsx
+++ client/components/RegisterUser/RegisterUser.test.tsx
@@ -1,52 +1,18 @@
-import { vi, test, expect, afterEach } from 'vitest'
-import { screen, render, cleanup } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+/**
+ * @vitest-environment jsdom
+ */
+
+import { expect, test } from 'vitest'
+import { fireEvent, render } from '@testing-library/react'
 import RegisterUser from './RegisterUser'
 
-afterEach(cleanup)
+test('the input field', () => {
+  render(<RegisterUser />)
+  const input = document.querySelector('input') as HTMLInputElement | null
 
-test('When the form is submitted the handleSubmit function should be called with a form object', async () => {
-  // Create a variable to store the form object
-  let submittedForm: unknown = null
+  // input exists in the RegisterUser component
+  expect(input).toBeTruthy()
 
-  // Define a mock handleSubmit function
-  const handleSubmitMock = vi.fn((form: unknown) => {
-    submittedForm = form
-  })
-
-  // Create a mock props object
-  const props = {
-    handleSubmit: handleSubmitMock,
-  }
-
-  // Render the component with the mock props
-  render(<RegisterUser {...props} />)
-
-  // Find the form elements
-  const firstNameInput = screen.getByLabelText(/First Name/i)
-  const lastNameInput = screen.getByLabelText(/Last Name/i)
-  const suburbSelect = screen.getByLabelText(/Suburb/i)
-  const submitButton = screen.getByRole('button', { name: /Register/i })
-
-  // Enter values in the form inputs
-  userEvent.type(firstNameInput, 'John')
-  userEvent.type(lastNameInput, 'Doe')
-  userEvent.selectOptions(suburbSelect, 'Auckland Central')
-
-  // Submit the form
-  userEvent.click(submitButton)
-
-  // Assert that the handleSubmit function is called with the expected form object
-  expect(handleSubmitMock).toHaveBeenCalledWith(expect.objectContaining({
-    firstName: 'John',
-    lastName: 'Doe',
-    suburb: 'Auckland Central',
-  }))
-
-  // Assert that the submitted form object is stored correctly
-  expect(submittedForm).toEqual(expect.objectContaining({
-    firstName: 'John',
-    lastName: 'Doe',
-    suburb: 'Auckland Central',
-  }))
+  // is empty
+  expect(input?.textContent).toBe('')
 })
