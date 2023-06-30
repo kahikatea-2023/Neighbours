ae1369679aaca754d436d54902e1a9aa435445bc
Jane Zhang <jiejanezhang2012@gmail.com>
2023-06-30 22:59:28 +1200

    added navigate to community page

--- client/components/RegisterUser/RegisterUser.tsx
+++ client/components/RegisterUser/RegisterUser.tsx
@@ -1,9 +1,11 @@
-import { ChangeEvent, FormEvent, useState } from 'react'
+import { useState } from 'react'
 import { UsersDataBackend } from '../../../models/user'
-import { useMutation, useQueryClient } from 'react-query'
-import { LocationsData, LocationsDataBackend } from '../../../models/locations'
+import { useNavigate } from 'react-router-dom'
+// save for later
+// import { useMutation, useQueryClient } from 'react-query'
 
 function RegisterUser() {
+  // for later when connect to backend
   // const queryClient = useQueryClient()
   // const mutations = useMutation(addNewUser, {
   //   onSuccess: () => {
@@ -11,7 +13,16 @@ function RegisterUser() {
   //   }
   // })
 
-  const [userData, setUserData] = useState({} as Partial<UsersDataBackend>)
+  const navigate = useNavigate()
+
+  const initialState = {
+    first_name: '',
+    last_name: '',
+    name: '',
+    location_id: 0,
+  } as Partial<UsersDataBackend>
+
+  const [userData, setUserData] = useState(initialState)
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const name = event.target.name
@@ -22,13 +33,20 @@ function RegisterUser() {
 
   function handleSelect(event: React.ChangeEvent<HTMLSelectElement>) {
     const value = +event.target.value
-    const newUser: LocationsDataBackend = { ...newUser, id: value }
-    setUserData(newUserData)
+    const currentUserData: Partial<UsersDataBackend> = { ...userData, location_id: value }
+    setUserData(currentUserData)
   }
 
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    // code below save for later
+    // mutations.mutate(userData)
+    //the redirect url need more work
+    const location = data.find((location) => location.id === userData.location_id)
+    const lowercaseName = location?.name.toLowerCase()
+    navigate(`/${lowercaseName}`)
+
   }
 
   // Hardcoded locations data
@@ -70,35 +88,30 @@ function RegisterUser() {
           <input
             type="text"
             name='firstName'
-            value={newUser.first_name}
+            value={userData.first_name}
             onChange={handleChange}
           />
         </div>
         <div>
           <label htmlFor='lastName'>Last Name</label>
-          <input type="text" name='lastName' value={newUser.last_name} onChange={handleChange} />
+          <input type="text" name='lastName' value={userData.last_name} onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="userName">User Name</label>
-          <input type="text" name='userName' value={newUser.name} onChange={handleChange} />
+          <input type="text" name='userName' value={userData.name} onChange={handleChange} />
         </div>
-
-
-
-        <label htmlFor='location'>Location</label>
-
-        <select value={newUser.location_id} onChange={handleSuburbChange}>
-          <option value="">Select Suburb</option>
-          {data.map((suburb) => (
-            <option key={suburb} value={suburb}>
-              {suburb}
-            </option>
-          ))}
-          {/*Render list of suburbs*/}
-        </select>
-
-        <br />
-        <button type="submit">Register</button>
+        <div>
+          <label htmlFor='location'>Location</label>
+          <select name='location' value={userData.location_id} onChange={handleSelect}>
+            <option value="">Select location</option>
+            {data.map((suburb) => (
+              <option key={suburb.id} value={suburb.id}>
+                {suburb.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="submit" className='w-auto bg-lightGreen text-white font-bold py-2 px-4 rounded-lg hover:shadow-[0px_0px_9px_2px_#65768C] drop-shadow-2xl'>Register</button>
       </form>
     </div>
   )
