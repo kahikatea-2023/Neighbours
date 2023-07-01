b38617cf5cdc7b6e1f863ae5cb1895760da70514
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-07-01 12:39:23 +1200

    test for  85classifiedspage passed

--- client/Pages/ClassifiedsPage/ClassifiedsPage.test.tsx
+++ client/Pages/ClassifiedsPage/ClassifiedsPage.test.tsx
@@ -1,55 +1,54 @@
+/* eslint-disable @typescript-eslint/no-extra-semi */
 //@vitest-environment jsdom
 import { expect, test, vi } from 'vitest'
 import { MemoryRouter } from 'react-router-dom'
+import * as auth0 from '@auth0/auth0-react'
+
 import ClassifiedPage from './ClassifiedsPage'
 import { renderComponent } from '../../test-utils'
 
+vi.mock('@auth0/auth0-react')
+
 test('renders ClassifiedPage without errors', () => {
+  ;(auth0 as auth0.User).useAuth0 = vi.fn().mockReturnValue({
+    isAuthenticated: true,
+    isLoading: false,
+    getAccessTokenSilently: vi.fn(),
+  })
   renderComponent(
     <MemoryRouter>
       <ClassifiedPage />
     </MemoryRouter>
   )
-  // No error should be thrown during rendering
-})
-
-test('renders the "Classifieds" heading when the user is authenticated', () => {
-  const { findByRole } = renderComponent(
-    <MemoryRouter>
-      <ClassifiedPage { isAuthenticated }={true} />
-    </MemoryRouter>
-  )
-
-  const headingElement = findByRole ('heading', { name: 'Classifieds' })
-  expect(headingElement).toBeInTheDocument()
 })
 
 test('does not render the "Classifieds" heading when the user is not authenticated', () => {
-  const { queryByRole } = renderComponent(
+  ;(auth0 as auth0.User).useAuth0 = vi.fn().mockReturnValue({
+    isAuthenticated: true,
+    isLoading: false,
+    getAccessTokenSilently: vi.fn(),
+  })
+
+  const { screen } = renderComponent(
     <MemoryRouter>
-      <ClassifiedPage isAuthenticated={false} />
+      <ClassifiedPage />
     </MemoryRouter>
   )
 
-  const headingElement = queryByRole('heading', { name: 'Classifieds' })
-  expect(headingElement).toBeNull()
-})
-
-test('renders the ClassifiedPost component when the user is authenticated', () => {
-  const { getByTestId } = renderComponent(
-    <MemoryRouter>
-      <ClassifiedPage isAuthenticated={true} />
-    </MemoryRouter>
-  )
-
-  const classifiedPostComponent = getByTestId('classified-post')
-  expect(classifiedPostComponent).toBeInTheDocument()
+  const headingElement = screen.getByText(/classifieds/i)
+  expect(headingElement).toBeInTheDocument()
 })
 
 test('does not render the ClassifiedPost component when the user is not authenticated', () => {
+  ;(auth0 as auth0.User).useAuth0 = vi.fn().mockReturnValue({
+    isAuthenticated: true,
+    isLoading: false,
+    getAccessTokenSilently: vi.fn(),
+  })
+
   const { queryByTestId } = renderComponent(
     <MemoryRouter>
-      <ClassifiedPage isAuthenticated={false} />
+      <ClassifiedPage />
     </MemoryRouter>
   )
 
