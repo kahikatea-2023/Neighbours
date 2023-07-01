e1267721d04ef0ae374b170036ec7e217524dd64
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-07-01 14:38:34 +1200

    start 92testnavbar

---
+++ client/components/Nav/Nav.test.tsx
@@ -0,0 +1,13 @@
+//@vitest-environment jsdom
+import { describe, it, expect, vi } from 'vitest'
+import { useAuth0 } from '@auth0/auth0-react'
+import { renderComponent } from '../../test-utils'
+import Nav from './Nav'
+import { screen } from '@testing-library/react'
+
+describe('Nav', () => {
+  it('should navigate to the home page when "Home" button is clicked', async () => {
+  
+
+  })
+})
