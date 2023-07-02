967e23f5e1adb9dba5d7558fc699935abaccf53b
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-07-02 15:14:02 +1200

    update comment component

--- client/Pages/ClassifiedsDetail/ClassifiedsDetail.tsx
+++ client/Pages/ClassifiedsDetail/ClassifiedsDetail.tsx
@@ -1,15 +1,10 @@
 import { useParams } from 'react-router-dom'
 import ClassifiedsDetailPost from '../../components/Classifieds/ClassifiedsDetailPost'
-import Comment from '../../components/Comment/Comment'
 
 function ClassifiedsDetail() {
   const location = useParams()
 
-  return
-  ;<div>
-    <ClassifiedsDetailPost />
-    <Comment />
-  </div>
+  return <ClassifiedsDetailPost />
 }
 
 export default ClassifiedsDetail
