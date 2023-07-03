0bbbab608c4be15908d13cd167773ef5a7cd2d87
Jaelyn <leejay1020@gmail.com>
2023-07-03 14:13:56 +1200

    search bar added to the classified page

--- client/Pages/ClassifiedsPage/ClassifiedsPage.tsx
+++ client/Pages/ClassifiedsPage/ClassifiedsPage.tsx
@@ -1,27 +1,44 @@
 import ClassifiedPost from '../../components/ClassifiedPost/ClassifiedPost'
 import { useAuth0 } from '@auth0/auth0-react'
-import { useQuery } from 'react-query'
 import { fetchClassifiedPost } from '../../apis/classifiedPost'
 import { useParams } from 'react-router-dom'
+import { useState } from 'react'
 
 function ClassifiedPage() {
   const locationId = Number(useParams())
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
+  const { isAuthenticated } = useAuth0()
+  const [ searchTerm, setSearchTerm ] = useState('')
 
-  const classifiedData = fetchClassifiedPost(locationId)
+  const classifiedData =  fetchClassifiedPost(locationId)
   console.log(classifiedData, 'I am in the classifiedPost')
 
   // const { isLoading, data } = useQuery('fetchLocations', async () => {
   //   return await fetchClassifiedPost()
   // })
 
+  function handleSearch(e: any) {
+    e.preventDefault()
+    console.log('search is here: ', searchTerm)
+  }
+
   return (
     isAuthenticated && (
       <div className="h-screen">
-        <div className="w-3/4 text-center text-4xl font-bold pl-7 mt-6 mb-6">
-          <h1>Classifieds</h1>
+        <div className="w-3/4 text-center text-4xl font-bold pl-7 mt-2 mb-6">
+          <h1 className="font-semibold text-2xl text-start">Classifieds</h1>
+          <h2 className="font-light text-base text-start">
+            Ask for help to your neighbours
+          </h2>
         </div>
         <ClassifiedPost />
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Search posts..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
       </div>
     )
   )
