e1c563cf77005bbadea060dc2c2f0cb60c5b64f2
Tem <tem.pearse01@gmail.com>
2023-07-04 18:24:33 +1200

    consume backend route

--- client/apis/comments.ts
+++ client/apis/comments.ts
@@ -8,38 +8,19 @@ export async function fetchComments(
   postId: number,
   token: string
 ) {
-  const mockData = Promise.resolve([
-    {
-      id: '1',
-      name: 'John Doe',
-      comment: 'This is the first comment.',
-    },
-    {
-      id: '2',
-      name: 'Jane',
-      comment: 'Here is another comment.',
-    },
-  ])
   const url = `${rootUrl}locations/${locationId}/classifieds/${postId}`
   const res = await request
     .get(url)
     .set('Authorization', `Bearer ${token}`)
     .set('Content-Type', 'application/json')
-  console.log('I am in the api call', res.body.comments)
-    return res.body.answers as typeof mockData
+
+  return res.body.answers as AnswerDataBackend[]
 }
 
-// comment: string;
-// classified_request_id: number;
-// user_name: string;
-// id: number;
-
-
-// export async function fetchComments(token: string,locationId:number, request:number) {
-//   const res = await request
-//   res.get(`/api/v1/${locationId}/classifieds/${request}`)
-//     .set('Authorization', `Bearer ${token}`)
-//     .set('Content-Type', 'application/json')
-
-//   return res.body.comments as []
-// }
+export async function deleteComment(commentId: number, token: string) {
+  const url = `${rootUrl}/comments/${commentId}`
+  await request
+    .get(url)
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+}
