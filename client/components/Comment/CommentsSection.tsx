debf7b84ba0fd64edda1e023d42ba514471349d4
Tem <tem.pearse01@gmail.com>
2023-07-04 18:50:51 +1200

    consume delete and revalidate

--- client/components/Comment/CommentsSection.tsx
+++ client/components/Comment/CommentsSection.tsx
@@ -1,53 +1,46 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { useState } from 'react'
-import { useQuery } from 'react-query'
-import { useParams } from 'react-router-dom'
-import { AnswerDataBackend } from '../../../models/comments'
-import { fetchClassifiedPost } from '../../apis/classifiedPost'
-import { fetchComments } from '../../apis/comments'
+import { useQuery, useQueryClient } from 'react-query'
+import { deleteComment, fetchComments } from '../../apis/comments'
 
 interface Props {
-  postId: string,
+  postId: string
   locationId: string
 }
 
-function CommentsSection({postId, locationId} : Props) {
+function CommentsSection({ postId, locationId }: Props) {
   const { getAccessTokenSilently } = useAuth0()
 
-  const { isLoading, data } = useQuery(
-    ['comments', postId],
-    async () => {
-      const token = await getAccessTokenSilently()
+  const client = useQueryClient()
+  const { data } = useQuery(['comments', postId], async () => {
+    const token = await getAccessTokenSilently()
 
-      return await fetchComments(Number(locationId), Number(postId), token)
-    }
-  )
+    return await fetchComments(Number(locationId), Number(postId), token)
+  })
 
-function handleDelete(commentId: number) {
- console.log(commentId)
-}
+  async function handleDelete(commentId: number) {
+    const token = await getAccessTokenSilently()
+    await deleteComment(commentId, token)
+    client.invalidateQueries(['comments'])
+  }
 
   return (
-     <div>
-          {/* Render the comment section */}
-          <h3>Comments</h3>
-          {data && data.map((comment) => (
-            <div key={comment.id}>
-              <p>
-                <strong>{comment.user_name}</strong>: {comment.comment}
-              </p>
-              <button onClick={() => handleDelete(comment.id)}>
-                Delete
-              </button>
-            </div>
-          ))}
-          <div>
-            <input type="text" placeholder="Add a comment" />
-            <button onClick={() => {}}>
-              Add Comment
-            </button>
+    <div>
+      {/* Render the comment section */}
+      <h3>Comments</h3>
+      {data &&
+        data.map((comment) => (
+          <div key={comment.id}>
+            <p>
+              <strong>{comment.user_name}</strong>: {comment.comment}
+            </p>
+            <button onClick={() => handleDelete(comment.id)}>Delete</button>
           </div>
-        </div>
+        ))}
+      <div>
+        <input type="text" placeholder="Add a comment" />
+        <button onClick={() => {}}>Add Comment</button>
+      </div>
+    </div>
   )
 }
 
