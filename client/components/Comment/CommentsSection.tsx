b9b7da38439228d101da826f02ad0573983fd83b
Jaelyn <leejay1020@gmail.com>
2023-07-05 15:17:06 +1200

    comment section styling done

--- client/components/Comment/CommentsSection.tsx
+++ client/components/Comment/CommentsSection.tsx
@@ -35,21 +35,40 @@ function CommentsSection({ postId, locationId }: Props) {
   }
 
   return (
-    <div>
-      {/* Render the comment section */}
-      <h3>Comments</h3>
+    <div className="p-4">
+      <h3 className="font-semi bold text-2xl mb-2">Comments</h3>
       {data &&
         data.map((comment) => (
-          <div key={comment.id}>
-            <p>
-              <strong>{comment.name}</strong>: {comment.comment}
-            </p>
-            <button onClick={() => handleDelete(comment.id)}>Delete</button>
+          <div
+            key={comment.id}
+            className="flex justify-between px-4 border-2 rounded-3xl border-pink mb-4 bg-lightPink"
+          >
+            <div className=" w-">
+              <p className="py-1">
+                <strong>{comment.name}</strong>
+              </p>
+              <p className="pb-2">{comment.comment}</p>
+            </div>
+            <div className="px-4 flex justify-end">
+              <button onClick={() => handleDelete(comment.id)}>
+                <img src="/images/delete.png" alt="delete" className="w-10" />
+              </button>
+            </div>
           </div>
         ))}
-      <form onSubmit={handleAdd}>
-        <input type="text" name="comment" placeholder="Add a comment" />
-        <button type="submit">Add Comment</button>
+      <form
+        onSubmit={handleAdd}
+        className="border-1 drop-shadow-lg rounded-full bg-white flex items-center"
+      >
+        <input
+          type="text"
+          name="comment"
+          placeholder="Write a comment ..."
+          className="pl-4"
+        />
+        <button type="submit">
+          <img src="/images/send.png" alt="send" className="w-5" />
+        </button>
       </form>
     </div>
   )
